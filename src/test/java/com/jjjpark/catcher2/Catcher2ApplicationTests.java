b42e736592e3890ae7ee23e6a4c7da157ffe5e1c
jhoon.bak <wecube.1@gmail.com>
2019-06-14 13:26:28 +0900

    test7

--- src/test/java/com/jjjpark/catcher2/Catcher2ApplicationTests.java
+++ src/test/java/com/jjjpark/catcher2/Catcher2ApplicationTests.java
@@ -37,10 +37,6 @@ public class Catcher2ApplicationTests {
         assertThat(1).isEqualTo(1);
     }
 
-//    @Test
-//    public void test_fail() {
-//        fail("fail....");
-//    }
 //
 //    @Test
 //    public void test_fail_2() {
